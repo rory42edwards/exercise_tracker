f7f561178d27d4c2224d8a4a6f56738bd4983e3a
rory42edwards <rory42edwards@gmail.com>
2024-06-11 01:54:53 +0100

    Add some more functionality

--- exercise_tracker/main.py
+++ exercise_tracker/main.py
@@ -9,11 +9,20 @@ def main():
 
     try:
         tracker.load_from_file('exercises.json')
-        print("exercise.json loaded successfully.")
+        print("exercises.json loaded successfully.")
     except FileNotFoundError:
-        print("exercise.json not found. Starting a new one.")
+        print("exercises.json not found. Starting a new one.")
+
+    # check for duplicate date in datebase
+    for exercise in tracker.exercises:
+        if date == exercise.date:
+            response = input("Date already found\
+ in database! Are you sure you want to continue? [y/n] ")
+            if response.lower() == 'n':
+                exit()
 
     while True:
+        # print options to user
         print("1. Add new exercise")
         print("2. Add set to exercise")
         print("3. Show all exercises")
@@ -24,13 +33,26 @@ def main():
             name = input("Enter exercise name: ")
             tracker.add_exercise(name)
         elif choice == '2':
-            name = input("Enter exercise name: ")
             tracker.show_exercises()
+            name = input("Enter exercise name: ")
             exercise = tracker.get_exercise(name)
             if exercise:
                 reps = int(input("Enter number of reps: "))
                 load = input("Enter load: ")
                 exercise.add_set(reps, load)
+                set_counter = 1
+                while True:
+                    more_sets = input("Add multiple sets? [y/n] ")
+                    if more_sets.lower() == 'n':
+                        break
+                    exercise.add_set(reps, load)
+                    set_counter += 1
+                    print("Adding another set...")
+                if set_counter == 1:
+                    print(f"Added {set_counter} set to {name}.")
+                else:
+                    print(f"Added {set_counter} sets to {name}.")
+
             else:
                 print("Exercise not found. Add it to the list.")
         elif choice == '3':
